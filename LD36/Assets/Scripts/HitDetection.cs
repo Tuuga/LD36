b10eea7d9e955e294f7731937fcd740f2b1903fa
Tuukka Muukkonen <tuukka.muukkonen@opisto.hkro>
2016-08-27 18:33:32 +0300

    Target prefab with movement and hit detection

---
+++ LD36/Assets/Scripts/HitDetection.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitDetection : MonoBehaviour {
+
+	public enum ColliderType { Head, Body };
+	public ColliderType ownType;
+
+	TargetCollision tc;
+	void Start () {
+		tc = transform.GetComponentInParent<TargetCollision>();
+	}
+
+	void OnCollisionEnter(Collision c) {
+		if (ownType == ColliderType.Head) {
+			tc.Hit(true);
+		} else {
+			tc.Hit(false);
+		}
+	}
+}
---
+++ LD36/Assets/Scripts/TargetCollision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetCollision : MonoBehaviour {
+
+	public int timesShotHead;
+	public int timesShotBody;
+
+	public void Hit(bool head) {
+		if (head) {
+			timesShotHead++;
+		} else {
+			timesShotBody++;
+		}
+	}
+}
