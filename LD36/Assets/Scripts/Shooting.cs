5812a7edba0f4e0e7c6c545597db4de482c2e871
Tuukka Muukkonen <tuukka.muukkonen@opisto.hkro>
2016-08-27 16:51:12 +0300

    Shooting script and projectile prefab

---
+++ LD36/Assets/Scripts/Shooting.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooting : MonoBehaviour {
+
+	public GameObject projectile;
+	public float force;
+	public float upForce;
+
+	GameObject mainCam;
+	Transform shootPos;
+
+	void Start () {
+		mainCam = GameObject.FindGameObjectWithTag("MainCamera");
+		shootPos = transform.Find("Shoot Point");
+	}
+
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) {
+			Shoot();
+		}
+	}
+
+	void Shoot () {
+		GameObject projIns = (GameObject)Instantiate(projectile, shootPos.position, Quaternion.identity);
+		var rb = projIns.GetComponent<Rigidbody>();
+		rb.velocity = (mainCam.transform.forward + Vector3.up * upForce).normalized * force;
+	}
+}
