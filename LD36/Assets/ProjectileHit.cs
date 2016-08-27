b2bb1ff6e4b72f6530c3a2f018ec6a0fe82fddca
Tomas Valkonen <tomas.valkonen@opisto.hkro>
2016-08-27 21:30:55 +0300

    Fabric audio. Weapon sounds

---
+++ LD36/Assets/ProjectileHit.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileHit : MonoBehaviour {
+
+    public string fabricEvent;
+
+    void OnCollisionEnter(Collision col)
+    {
+        Fabric.EventManager.Instance.PostEvent(fabricEvent, gameObject);
+    }
+}
--- LD36/Assets/Scripts/Shooting.cs
+++ LD36/Assets/Scripts/Shooting.cs
@@ -6,6 +6,7 @@ public class Shooting : MonoBehaviour {
 	public GameObject projectile;
 	public float force;
 	public float upForce;
+    public string fabricEvent;
 
 	GameObject mainCam;
 	Transform shootPos;
@@ -18,12 +19,15 @@ public class Shooting : MonoBehaviour {
 	void Update () {
 		if (Input.GetButtonDown("Fire1")) {
 			Shoot();
-		}
+        }
 	}
 
 	void Shoot () {
-		GameObject projIns = (GameObject)Instantiate(projectile, shootPos.position, Quaternion.identity);
+        Fabric.EventManager.Instance.PostEvent(fabricEvent, gameObject);
+        GameObject projIns = (GameObject)Instantiate(projectile, shootPos.position, Quaternion.identity);
 		var rb = projIns.GetComponent<Rigidbody>();
 		rb.velocity = (mainCam.transform.forward + Vector3.up * upForce).normalized * force;
+
+        
 	}
 }
