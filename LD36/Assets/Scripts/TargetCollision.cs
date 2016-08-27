1ef9027e931e99ad5a9a8502f19f1bf1baac4c79
Tuukka Muukkonen <tuukka.muukkonen@opisto.hkro>
2016-08-27 20:47:05 +0300

    Target activation and death

--- LD36/Assets/Scripts/TargetCollision.cs
+++ LD36/Assets/Scripts/TargetCollision.cs
@@ -3,14 +3,20 @@ using System.Collections;
 
 public class TargetCollision : MonoBehaviour {
 
-	public int timesShotHead;
-	public int timesShotBody;
+	int timesShotHead;
+	int timesShotBody;
+
+	public int headHP;
+	public int bodyHP;
 
 	public void Hit(bool head) {
 		if (head) {
 			timesShotHead++;
 		} else {
 			timesShotBody++;
+		}
+		if (timesShotHead >= headHP || timesShotBody >= bodyHP) {
+			GetComponent<TargetController>().Die();
 		}
 	}
 }
---
+++ LD36/Assets/Scripts/TargetController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetController: MonoBehaviour {
+
+	public float speed;
+	public string actAnim;
+	string deactAnim;
+
+	Vector3 pointA;
+	Vector3 pointB;
+	Vector3 nextPoint;
+
+	bool activated;
+
+	Transform targetParts;
+
+	Animator anim;
+	void Start () {
+		pointA = transform.position;
+		pointB = transform.Find("Point B").position;
+		targetParts = transform.Find("Target Parts");
+		anim = targetParts.GetComponent<Animator>();
+		deactAnim = actAnim + "R";
+	}
+
+	void Update () {
+		if (Vector3.Distance(targetParts.position, pointA) < 0.2f) {
+			nextPoint = pointB;
+		} else if (Vector3.Distance(targetParts.position, pointB) < 0.2f) {
+			nextPoint = pointA;
+		}
+
+		Vector3 dir = (nextPoint - targetParts.position).normalized;
+		targetParts.position += dir * speed * Time.deltaTime;
+	}
+
+	void Activate () {
+		activated = true;
+		anim.Play(actAnim);
+		print("Activated");
+	}
+
+	void Deactivate () {
+		anim.Play(deactAnim);
+		activated = false;
+	}
+
+	public void Die () {
+		anim.Play(deactAnim);
+	}
+
+	// Can only collide with player layer
+	void OnTriggerEnter(Collider c) {
+		if (!activated) {
+			Activate();
+		}
+	}
+}
