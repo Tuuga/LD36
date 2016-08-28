df9beceaa0a1707e04cdc7e0899906f061605b48
Tuukka Muukkonen <tuukka.muukkonen@opisto.hkro>
2016-08-28 18:08:33 +0300

    Lazergun logic done

---
+++ LD36/Assets/Scripts/Lazer_Shooting.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lazer_Shooting : MonoBehaviour {
+
+	public float reloadTime;
+	public float recoil;
+	public float force;
+	public LayerMask mask;
+	public GameObject projectile;
+
+	public string fabricEvent;
+
+	GameObject mainCam;
+	float reloadTimer;
+
+	Transform shootPos;
+
+	void Start() {
+		mainCam = GameObject.FindGameObjectWithTag("MainCamera");
+		shootPos = GameObject.Find("Shoot Point").transform;
+	}
+
+
+	void Update() {
+		reloadTimer -= Time.deltaTime;
+		if (Input.GetButtonDown("Fire1") && transform.parent.tag == "Player") {
+			Shoot();
+		}
+	}
+
+	void Shoot() {
+		if (reloadTimer >= 0)
+			return;
+
+		reloadTimer = reloadTime;		
+
+		Fabric.EventManager.Instance.PostEvent(fabricEvent, gameObject);
+		var dir = mainCam.transform.forward;
+		float aimX = Random.Range(-recoil, recoil);
+		float aimY = Random.Range(-recoil, recoil);
+		Quaternion aimRot = Quaternion.Euler(aimX, aimY, 0);
+		dir = aimRot * dir;
+
+		GameObject projIns = (GameObject)Instantiate(projectile, shootPos.position, mainCam.transform.rotation);
+		var rb = projIns.GetComponent<Rigidbody>();
+		rb.velocity = dir * force;
+
+		RaycastHit hit;
+		if (Physics.Raycast(mainCam.transform.position, dir, out hit, Mathf.Infinity, mask)) {
+			if (hit.transform.tag == "Target") {
+				hit.transform.GetComponent<HitDetection>().GotHit();
+			}
+			Debug.DrawLine(mainCam.transform.position, hit.point);
+		}
+	}
+}
--- LD36/Assets/Scripts/ProjectileHit.cs
+++ LD36/Assets/Scripts/ProjectileHit.cs
@@ -4,11 +4,16 @@ using System.Collections;
 public class ProjectileHit : MonoBehaviour {
 
 	public string fabricEvent;
+	public bool destroyOnHit;
 	bool hit;
+
 	void OnCollisionEnter(Collision col) {
-		if (!hit) {
+		if (!hit && fabricEvent != "") {
 			Fabric.EventManager.Instance.PostEvent(fabricEvent, gameObject);
 			hit = true;
+		}
+		if (destroyOnHit) {
+			Destroy(gameObject);
 		}
 	}
 }
