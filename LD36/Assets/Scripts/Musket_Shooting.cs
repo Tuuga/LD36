2f74aa0afd0ed447038f65f0ab1c976b3774ae02
Tuukka Muukkonen <tuukka.muukkonen@opisto.hkro>
2016-08-28 17:32:18 +0300

    Weapon prefabs reorganized, musket logic done

---
+++ LD36/Assets/Scripts/Musket_Shooting.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Musket_Shooting : MonoBehaviour {
+	public float reloadTime;
+	public float recoil;
+	public LayerMask mask;
+
+	public string fabricEvent;
+
+	GameObject mainCam;
+	float reloadTimer;
+
+	void Start() {
+		mainCam = GameObject.FindGameObjectWithTag("MainCamera");
+	}
+
+	void Update() {
+		reloadTimer -= Time.deltaTime;
+		if (Input.GetButtonDown("Fire1") && transform.parent.tag == "Player") {
+			Shoot();
+		}
+	}
+
+	void Shoot() {
+		if (reloadTimer >= 0)
+			return;
+
+		reloadTimer = reloadTime;
+
+		Fabric.EventManager.Instance.PostEvent(fabricEvent, gameObject);
+		var dir = mainCam.transform.forward;
+		float aimX = Random.Range(-recoil, recoil);
+		float aimY = Random.Range(-recoil, recoil);
+		Quaternion aimRot = Quaternion.Euler(aimX, aimY, 0);
+		dir = aimRot * dir;
+
+		RaycastHit hit;
+		if (Physics.Raycast(mainCam.transform.position, dir, out hit, Mathf.Infinity, mask)) {
+			if (hit.transform.tag == "Target") {
+				hit.transform.GetComponent<HitDetection>().GotHit();
+			}
+			Debug.DrawLine(mainCam.transform.position, hit.point);
+		}
+	}
+}
