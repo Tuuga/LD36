c23846e1c7f722ab554e33dec7fc8bae1c624fbd
Tuuga-PC\Tuuga <tuukka.o.muukkonen@gmail.com>
2016-08-29 19:18:05 +0300

    GameManager, FadeIn and FadeOut animations

---
+++ LD36/Assets/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject fadeObject;
+	Animator fadeAnim;
+	Image fadeImage;
+
+	GameObject player;
+	PlayerController pc;
+
+	void Start () {
+		fadeAnim = fadeObject.GetComponent<Animator>();
+		player = GameObject.FindGameObjectWithTag("Player");
+		pc = player.GetComponent<PlayerController>();
+
+		// Start seq
+		fadeAnim.Play("FadeIn");
+		// Play sound
+	}
+
+	public void GameOver () {
+		fadeAnim.Play("FadeOut");
+	}
+}
